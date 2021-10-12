023698472f382682d70523a2ec3ec69ea7e1b656
SyedHasibRahman <syedhasib01@gmail.com>
2021-10-12 19:12:12 +0600

    Add slider using react-bootstrap carousel

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Expert.css'
 
 const Expert = (props) => {
-    const { id, name, price, expertize, img } = props.expert;
+    const {  name, expertize, img } = props.expert;
     return (
         <div className="col col-lg-4 col-md-6 col-sm-6 col-12 p-3 ">
             <img className="expert-img" src={img} alt="" />
